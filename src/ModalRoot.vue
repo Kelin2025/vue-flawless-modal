ff7b99d1dd50baa00966a92f0b576d3ffcf37874
Kelin2025 <kelin2025@yandex.ru>
2018-03-07 01:46:51 +0300

    ModalRoot component

---
+++ src/ModalRoot.vue
@@ -0,0 +1,17 @@
+<template>
+  <portal-target slim multiple :name="name" />
+</template>
+
+<script>
+import PortalTarget from 'portal-vue/src/components/portal-target'
+
+export default {
+  components: { PortalTarget },
+  props: {
+    name: {
+      type: String,
+      default: 'modal-root'
+    }
+  }
+}
+</script>
