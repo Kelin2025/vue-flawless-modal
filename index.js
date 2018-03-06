33447ac00d7d0aff9402fa65437aef5440679760
Kelin2025 <kelin2025@yandex.ru>
2018-03-07 01:46:41 +0300

    Created index.js with modal bus and installer

---
+++ index.js
@@ -0,0 +1,33 @@
+import NanoEvents from 'nanoevents'
+import ModalRoot from './src/ModalRoot'
+import ModalWindow from './src/ModalWindow'
+
+const bus = new NanoEvents()
+
+export const modal = {
+  on(evt, cb) {
+    return bus.on(evt, cb)
+  },
+  emit(evt, ...data) {
+    return bus.emit(evt, ...data)
+  },
+  open(name, context) {
+    return bus.emit(`modal.open:${name}`, context)
+  },
+  send(name, context) {
+    return bus.emit(`modal.send:${name}`, context)
+  },
+  close(name) {
+    return bus.emit(`modal.close:${name}`)
+  }
+}
+
+export default Vue => {
+  Vue.component('modal-root', ModalRoot)
+  Vue.component('modal-window', ModalWindow)
+  Vue.mixin({
+    created() {
+      this.$modal = modal
+    }
+  })
+}
